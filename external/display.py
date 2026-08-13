57115d82fb071e7a3ac1285d98cd5b0ac6aa8dad
Zach Hagelberg <zachhagelberg7@gmail.com>
2026-08-12 20:20:39 -0700

    display.py shows point cloud

--- external/display.py
+++ external/display.py
@@ -1,19 +1,25 @@
-"""import time
+import time
 from pathlib import Path
 from xmlrpc import server
 
 import numpy as np
 import viser
 import serial
+import serial.tools.list_ports
 import viser.transforms as tf
 
 import math
 
 ports = serial.tools.list_ports.comports()
-SerialPort = serial.Serial(port=ports[0].device"""'/dev/ttyACM0'""", baudrate = 115200)
+for port in ports:
+    if port.device == "/dev/ttyACM0":
+        SerialPort = serial.Serial(port=port.device, baudrate = 115200)
+    elif port.device == "/dev/ttyACM1":
+        SerialPort = serial.Serial(port=port.device, baudrate = 115200)
 SerialPort.flushInput()
 SerialPort.flushOutput()
 time.sleep(1)
+scaleUp = 10
 
 def main():
     server = viser.ViserServer()
@@ -21,7 +27,7 @@ def main():
         "/Points",
         points = np.ndarray((0, 3)),
         colors = (82, 46, 242),
-        point_size = 0.5
+        point_size = 0.1
     )
     allPoints = np.ndarray((0, 3))
     print("Open your browser to http://localhost:8080")
@@ -31,8 +37,8 @@ def main():
         Parsed = Input.split(",")
         if Parsed[0] == "NewPts":
             for i in range(1, int((len(Parsed)-1)/3)): # Start at 1 because 0th index is "NewPts"
-                allPoints.append([Parsed[3*i-2], Parsed[3*i-1], Parsed[3*i]])
+                allPoints = np.append(allPoints, [float(Parsed[3*i-2])*scaleUp, float(Parsed[3*i-1])*scaleUp, float(Parsed[3*i])*scaleUp])
             pointCloud.points = allPoints
 
 if __name__=="__main__":
-    main()"""
+    main()
